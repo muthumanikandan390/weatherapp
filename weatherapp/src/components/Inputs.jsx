b4ec181d8ff137a0b0ab6c3a8a48924cea9353a3
muthumanikandan390d <muthumanikandan390d@gmail.com>
2024-06-21 15:16:37 +0530

    added-ui2

--- weatherapp/src/components/Inputs.jsx
+++ weatherapp/src/components/Inputs.jsx
@@ -1,12 +1,22 @@
-
+import {BiSearch , BiCurrentLocation} from "react-icons/bi"
 
 const Inputs = () => {
   return (
-    <div>
-      <div>
-    <input type='text' placeholder='type here ...' className="text-gray-500"/>
-      </div>
-    </div>
+    <div className="flex flex-row my-6 justify-center">
+      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+      <input
+  type='text'
+  placeholder='type here ...'
+  className="text-gray-500 text-xl font-light
+  p-2 w-96 shadow-xl capitalize
+  focus:outline-none placeholder:lowercase"
+/>
+<BiSearch size={30} className="cursor-pointer transition ease-out hover:scale-125"/>
+<BiCurrentLocation size={30} className="cursor-pointer transition ease-out hover:scale-125"/>
+
+ </div>
+ <button>+</button>
+ </div>
   )
 }
 
